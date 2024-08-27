2c0dba09a0beed09b6d3a00807670b47e06acc7f
Kiarash Shahedi <puddin@kali>
2024-08-27 15:47:26 +0330

    making chat app models and other fields

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, related_name='messages', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content}'
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import ChatRoom, Message
+from .serializers import ChatRoomSerializer, MessageSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ChatRoomListCreateView(generics.ListCreateAPIView):
+    queryset = ChatRoom.objects.all()
+    serializer_class = ChatRoomSerializer
+    permission_classes = [IsAuthenticated]
+
+class MessageListCreateView(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
